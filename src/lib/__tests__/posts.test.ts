c559bbeaba13edec76d68cf0e0894f86f5fd570e
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2025-02-03 20:12:57 +0100

    feat(posts): change title and file name

--- src/lib/__tests__/posts.test.ts
+++ src/lib/__tests__/posts.test.ts
@@ -2,7 +2,7 @@ import { getPostFileContent } from '../posts';
 
 describe('getPostFileContent', () => {
   it('should get the files content correctly', () => {
-    const content = getPostFileContent('what-are-you-starting-this-year');
+    const content = getPostFileContent('first-post');
 
     expect(content).not.toBeUndefined();
   });
