760043a638521806ab29d86c8d82e8a2c04c1e18
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2025-02-03 19:58:59 +0100

    fix(posts.test): use correct post path

--- src/lib/__tests__/posts.test.ts
+++ src/lib/__tests__/posts.test.ts
@@ -2,7 +2,7 @@ import { getPostFileContent } from '../posts';
 
 describe('getPostFileContent', () => {
   it('should get the files content correctly', () => {
-    const content = getPostFileContent('first-post');
+    const content = getPostFileContent('what-are-you-starting-this-year');
 
     expect(content).not.toBeUndefined();
   });
